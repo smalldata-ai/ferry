cfb1f2d4acd130ee7fd844f3c1dc07d9a32ab0c6
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-02-21 16:46:19 +0530

    Update test_source_factory.py

--- ferry/tests/test_source_factory.py
+++ ferry/tests/test_source_factory.py
@@ -2,7 +2,14 @@ import pytest
 from ferry.src.exceptions import InvalidSourceException
 from ferry.src.sources.postgres_source import PostgresSource
 from ferry.src.source_factory import SourceFactory
+from ferry.src.sources.s3_source import S3Source  # Import the S3 source class
 
+
+def test_get_s3_source():
+    """Test if S3 source is correctly returned."""
+    uri = "s3://my-bucket/path/to/data.csv"
+    source = SourceFactory.get(uri)
+    assert isinstance(source, S3Source)
 
 
 def test_get_postgres_source():
@@ -11,7 +18,12 @@ def test_get_postgres_source():
     assert isinstance(source, PostgresSource)
 
 
-def test_throw_exception_source_not_found():
-    uri = 'http://username:password@localhost/dbname'
+def test_get_invalid_source():
+    """Test if an invalid source raises an exception."""
+    uri = "invalid://some_path"
     with pytest.raises(InvalidSourceException):
         SourceFactory.get(uri)
+
+
+
+
