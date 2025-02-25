088e61c8fd324b167a86bac31be9dcfcbd70f914
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-02-25 14:35:40 +0530

    Update source_factory.py

--- ferry/src/source_factory.py
+++ ferry/src/source_factory.py
@@ -3,12 +3,13 @@ from urllib.parse import urlparse
 from ferry.src.exceptions import InvalidSourceException
 from ferry.src.sources.postgres_source import PostgresSource
 from ferry.src.sources.source_base import SourceBase
-
+from ferry.src.sources.s3_source import S3Source
 
 class SourceFactory:
     _items = {
         "postgres": PostgresSource,
         "postgresql": PostgresSource,
+        "s3":S3Source,
     }
 
     @staticmethod
