913a4e7d8cd142e949df50ee2dda3369150da192
Shravani1383 <shravanimjagtap13@gmail.com>
2025-02-26 16:42:21 +0530

    postgres destination uri validation

--- ferry/src/destinations/postgres_destination.py
+++ ferry/src/destinations/postgres_destination.py
@@ -1,8 +1,16 @@
 import dlt
-
 from ferry.src.destinations.destination_base import DestinationBase
+from ferry.src.restapi.database_uri_validator import DatabaseURIValidator  # Import the centralized validator
 
 class PostgresDestination(DestinationBase):
 
-    def dlt_target_system(self, uri: str, **kwargs): # type: ignore
+    def validate_uri(self, uri: str):
+        """Use centralized URI validation for PostgreSQL."""
+        DatabaseURIValidator.validate_uri(uri)
+
+    def dlt_target_system(self, uri: str, **kwargs):  # type: ignore
+        # Validate the URI before proceeding
+        self.validate_uri(uri)
+
+        # Return the PostgreSQL destination for dlt
         return dlt.destinations.postgres(credentials=uri, **kwargs)
