b0e073b3792860a2e25adc07d269bab5dc33b097
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-02-26 08:54:13 +0530

    Update destination_factory.py

--- ferry/src/destination_factory.py
+++ ferry/src/destination_factory.py
@@ -2,10 +2,10 @@ from urllib.parse import urlparse
 
 from ferry.src.destinations.clickhouse_destination import ClickhouseDestination
 from ferry.src.destinations.postgres_destination import PostgresDestination
-from ferry.src.destinations.duckdb_destination import DuckDBDestination
-from ferry.src.destinations.s3_destination import S3Destination  # Import S3 destination
 from ferry.src.destinations.destination_base import DestinationBase
 from ferry.src.exceptions import InvalidDestinationException
+from ferry.src.destinations.duckdb_destination import DuckDBDestination
+
 
 
 class DestinationFactory:
@@ -13,8 +13,8 @@ class DestinationFactory:
         "postgres": PostgresDestination,
         "postgresql": PostgresDestination,
         "clickhouse": ClickhouseDestination,
-        "duckdb": DuckDBDestination,  
-        "s3": S3Destination  # Add S3 as a valid destination
+        "duckdb": DuckDBDestination,  # Added DuckDB support
+
     }
 
     @staticmethod
@@ -24,4 +24,4 @@ class DestinationFactory:
             class_ = DestinationFactory._items.get(fields.scheme, DestinationBase)
             return class_()
         else:
-            raise InvalidDestinationException(f"Invalid destination type: {fields.scheme}")
+            raise InvalidDestinationException()
